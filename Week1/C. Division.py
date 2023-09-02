58791a3724fef68d70385f77a69e88b032a1ed58
khaled_Omar <kmr444166@gmail.com>
2023-09-02 10:36:23 +0200

    Add new file

---
+++ Week1/C. Division.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    rating = int(input())
+    if rating >= 1900:
+        print("Division 1")
+    elif 1600 <= rating <= 1899:
+        print("Division 2")
+    elif 1400 <= rating <= 1599:
+        print("Division 3")
+    else:
+        print("Division 4")
